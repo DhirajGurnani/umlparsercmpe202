fb737b3045e32e41e696ce878109bc3c473cd1fd
DhirajGurnani <dhirajgurnani20@gmail.com>
2017-02-22 22:11:20 -0800

    getting names of classes by the directory

--- UML parser/src/main.java
+++ UML parser/src/main.java
@@ -1,21 +1,36 @@
+import japa.parser.ast.CompilationUnit;
+
+import java.io.File;
 import java.io.FileInputStream;
 
 import org.antlr.v4.runtime.*;
 
-import com.github.javaparser.JavaParser;
+/*import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.MethodDeclaration;
-import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;*/
 public class main {
 	public static void main(String[] args) throws Exception {
         // creates an input stream for the file to be parsed
-        FileInputStream in = new FileInputStream("b.java");
+        //FileInputStream in = new FileInputStream("b.java");
+		File in = new File("Test Case 1");
+		
+		File[] listOfClasses = in.listFiles();
+		for (int i = 0; i < listOfClasses.length; i++) {
+			String ClassName = listOfClasses[i].getName();
+			//String ext = fileName.substring(fileName.length() - 4);
+			if(ClassName.endsWith("java")){
+				System.out.println(ClassName);
+			}
+		}
 
+		//File fileTemp[] = in.listFiles((File pathName) -> pathName.getName().endsWith(".java"));
+		//CompilationUnit cUnit[] = new CompilationUnit[fileTemp.length];
         // parse it
-        CompilationUnit cu = JavaParser.parse(in);
+        //CompilationUnit cu = JavaParser.parse(in);
 
         // visit and print the methods names
-        new MethodVisitor().visit(cu, null);
+       // new MethodVisitor().visit(cu, null);
     }
 
     
