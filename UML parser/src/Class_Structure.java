67c0e37d3cb2342633686f0aade54e4a80eeaaab
DhirajGurnani <dhirajgurnani20@gmail.com>
2017-03-24 18:01:10 -0700

    adding the structure tab

---
+++ UML parser/src/Class_Structure.java
@@ -0,0 +1,48 @@
+import java.io.File;
+import java.util.ArrayList;
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+
+
+public class Class_Structure {
+	  static ArrayList<String> Class_Names = new ArrayList<String>();
+	  static ArrayList<String> Method_Names = new ArrayList<String>();
+	  static ArrayList<String> Attribute_Names = new ArrayList<String>();
+		public static ArrayList getClasses(File projectDir) {
+
+	        new DirExplorer((level, path, file) -> path.endsWith(".java"), (level, path, file) -> {
+	        	
+	          //  System.out.println(path);
+	           // System.out.println(Strings.repeat("=", path.length()));
+	            //try {
+	                try {
+						new VoidVisitorAdapter<Object>() {
+						    @Override
+						    public void visit(ClassOrInterfaceDeclaration n, Object arg) {
+						        super.visit(n, arg);
+						        
+						    
+						        //System.out.println(" * " + n.getNameAsString());
+						        //Class_Names.add(n.getName());
+						        Class_Names.add(n.getNameAsString());
+						        
+						        //String s = (() n.getName()).getSimpleName();
+						        //System.out.println(" * get members" + n.getMembers());
+						    }
+						}.visit(JavaParser.parse(file), null);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+	                
+	                new RuntimeException();
+	            
+	        }).explore(projectDir);
+	        
+	        return Class_Names;
+	        
+	        
+	    }
+
+}
