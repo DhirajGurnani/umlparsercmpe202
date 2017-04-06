efa0fb806b41038ee9c18d97ce81cd5086cbb07a
DhirajGurnani <dhirajgurnani20@gmail.com>
2017-04-05 17:50:53 -0700

    resolution of errors

--- UML parser/src/App.java
+++ UML parser/src/App.java
@@ -43,30 +43,3 @@ public class App
    }
 	    }
 	}
-new DirExplorer((level, path, file) -> path.endsWith(".java"), (level, path, file) -> {
-    System.out.println(path);
-    System.out.println(Strings.repeat("=", path.length()));
-    try {
-        new NodeIterator(new NodeIterator.NodeHandler() {
-            @Override
-            public boolean haandle1(Node node) {
-                if (node instanceof Statement) {
-                    System.out.println(" [Lines " + node.getBegin() + " - " + node.getEnd() + " ] " + node);
-                    return false;
-                } else {
-                    return true;
-                }
-            }
-
-			@Override
-			public boolean handle(Node node) {
-				// TODO Auto-generated method stub
-				return false;
-			}
-        }).explore(JavaParser.parse(file));
-        System.out.println(); // empty line
-    } catch (Exception e) {
-        new RuntimeException(e);
-    }
-}).explore(projectDir);
-
